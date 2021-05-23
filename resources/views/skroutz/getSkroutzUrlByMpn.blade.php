d7759c9e6e291b59154e6bedc2b6cb133ffac506
George Tekelis <tekelisg@gmail.com>
2021-05-23 15:21:50 +0300

    added skroutz url column to view

--- resources/views/skroutz/getSkroutzUrlByMpn.blade.php
+++ resources/views/skroutz/getSkroutzUrlByMpn.blade.php
@@ -40,9 +40,9 @@
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Εχει γινει επεξεργασια
                             </th>
-{{--                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">--}}
-{{--                                Role--}}
-{{--                            </th>--}}
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Skroutz Url
+                            </th>
                             <th scope="col" class="relative px-6 py-3">
                                 <span class="sr-only">Edit</span>
                             </th>
@@ -80,9 +80,9 @@
                                         {{$mpn->is_processed}}
                                     </span>
                                 </td>
-{{--                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">--}}
-{{--                                    Admin--}}
-{{--                                </td>--}}
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    ---
+                                </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                     <form action="{{ route('mpnList', $mpn) }}" method="post">
                                         @csrf
