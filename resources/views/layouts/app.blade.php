df4bb4628973aa57dd6605eb37d225450d8e7160
George Tekelis <tekelisg@gmail.com>
2021-05-26 13:09:40 +0300

    menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,12 +32,12 @@
 
                     <div class="dropdown inline-block relative">
                         <button class="bg-white  px-3 rounded inline-flex items-center">
-                            <span class="mr-1">Skroutz URLs</span>
+                            <span class="mr-1">Skroutz</span>
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
                         </button>
                         <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
-                            <li class=""><a href="{{route('mpnList')}}" class="rounded-t bg-gray-100 hover:bg-blue-100 py-2 px-4 block whitespace-no-wrap">Εύρεση με MPN</a></li>
-                            <li class=""><a href="{{route('eanList')}}" class="bg-gray-100 hover:bg-blue-100 py-2 px-4 block whitespace-no-wrap">Εύρεση με EAN</a></li>
+                            <li class=""><a href="{{route('mpnList')}}" class="rounded-t bg-gray-100 hover:bg-blue-100 py-2 px-4 block whitespace-no-wrap">Εύρεση URL με MPN</a></li>
+                            <li class=""><a href="{{route('eanList')}}" class="bg-gray-100 hover:bg-blue-100 py-2 px-4 block whitespace-no-wrap">Εύρεση URL με EAN</a></li>
 {{--                            <li class=""><a href="#" class="rounded-b bg-gray-100 hover:bg-blue-100 py-2 px-4 block whitespace-no-wrap">Placeholder</a></li>--}}
                         </ul>
                     </div>
