c4072375aa7d3bbe3c871d0f3d2307fb831afeba
George Tekelis <tekelisg@gmail.com>
2021-05-26 15:42:15 +0300

    same

--- resources/views/skroutz/productsToImportDataFromSkroutz.blade.php
+++ resources/views/skroutz/productsToImportDataFromSkroutz.blade.php
@@ -43,12 +43,12 @@
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Skroutz Url
                             </th>
-                            <th scope="col" class="relative px-6 py-3">
-                                <span class="sr-only">Edit</span>
-                            </th>
-                            <th scope="col" class="relative px-6 py-3">
-                                <span class="sr-only">Διαγραφή</span>
-                            </th>
+{{--                            <th scope="col" class="relative px-6 py-3">--}}
+{{--                                <span class="sr-only">Edit</span>--}}
+{{--                            </th>--}}
+{{--                            <th scope="col" class="relative px-6 py-3">--}}
+{{--                                <span class="sr-only">Διαγραφή</span>--}}
+{{--                            </th>--}}
                         </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
@@ -87,33 +87,33 @@
                                         --
                                     @endif
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <form action="" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-blue-600 hover:text-blue-900">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
-                                                 fill="currentColor">
-                                                <path
-                                                        d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/>
-                                            </svg>
-                                        </button>
-                                    </form>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <form action="" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-500 hover:text-red-700">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
-                                                 fill="currentColor">
-                                                <path fill-rule="evenodd"
-                                                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                      clip-rule="evenodd"/>
-                                            </svg>
-                                        </button>
-                                    </form>
-                                </td>
+{{--                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">--}}
+{{--                                    <form action="" method="post">--}}
+{{--                                        @csrf--}}
+{{--                                        @method('DELETE')--}}
+{{--                                        <button type="submit" class="text-blue-600 hover:text-blue-900">--}}
+{{--                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"--}}
+{{--                                                 fill="currentColor">--}}
+{{--                                                <path--}}
+{{--                                                        d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/>--}}
+{{--                                            </svg>--}}
+{{--                                        </button>--}}
+{{--                                    </form>--}}
+{{--                                </td>--}}
+{{--                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">--}}
+{{--                                    <form action="" method="post">--}}
+{{--                                        @csrf--}}
+{{--                                        @method('DELETE')--}}
+{{--                                        <button type="submit" class="text-red-500 hover:text-red-700">--}}
+{{--                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"--}}
+{{--                                                 fill="currentColor">--}}
+{{--                                                <path fill-rule="evenodd"--}}
+{{--                                                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"--}}
+{{--                                                      clip-rule="evenodd"/>--}}
+{{--                                            </svg>--}}
+{{--                                        </button>--}}
+{{--                                    </form>--}}
+{{--                                </td>--}}
                             </tr>
                         @endforeach
 
