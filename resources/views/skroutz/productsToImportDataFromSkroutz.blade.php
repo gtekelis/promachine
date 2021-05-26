d69b64684f2975e0e8aff1420ef789690fb38c71
George Tekelis <tekelisg@gmail.com>
2021-05-26 15:47:13 +0300

    added ascending numbering

--- resources/views/skroutz/productsToImportDataFromSkroutz.blade.php
+++ resources/views/skroutz/productsToImportDataFromSkroutz.blade.php
@@ -25,6 +25,10 @@
                         <tr>
                             <th scope="col"
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                #
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 MPN
                             </th>
                             <th scope="col"
@@ -56,23 +60,13 @@
                         @foreach($product_list as $i => $values)
                             <tr>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="flex items-center">
-                                        <div class="flex-shrink-0 h-10 w-10">
-                                            {{--                                            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">--}}
-                                        </div>
-                                        <div class="ml-4">
-                                            <div class="text-sm font-medium text-gray-900">
-                                                {{ $values['mpn'] }}
-                                            </div>
-                                            <div class="text-sm text-gray-500">
-                                                {{--jane.cooper@example.com--}}
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <div class="text-sm text-gray-900"> {{$loop->iteration}} </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900"> {{ $values['mpn'] }} </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="text-sm text-gray-900"> {{ $values['name'] }} </div>
-                                    {{--<div class="text-sm text-gray-500">Optimization</div>--}}
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="text-sm text-gray-900"> {{ $values['model'] }} </div>
