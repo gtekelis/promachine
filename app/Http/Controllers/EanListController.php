562e9b97ef5868350ea40f6851b88b336e37731e
George <tekelisg@gmail.com>
2021-05-31 22:21:02 +0300

    ean save to db

--- app/Http/Controllers/EanListController.php
+++ app/Http/Controllers/EanListController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EanList;
 use Illuminate\Http\Request;
 
 class EanListController extends Controller
 {
     public function index(){
-        return view('skroutz.getSkroutzUrlByEan');
+        $ean_list = EanList::paginate(50);
+        return view('skroutz.getSkroutzUrlByEan',[
+            'ean_list' => $ean_list
+        ]);
+    }
+
+    public function insertEanListToDb(Request $request ){
+        $this->validate($request, [
+            'ean' => 'required'
+        ]);
+
+        $ean_string = trim($request->ean);
+        $ean_array = explode("\n",$ean_string);
+
+        for ($i = 0; $i < count($ean_array); $i++){
+            EanList::create([
+                'ean' => trim($ean_array[$i])
+            ]);
+        }
+        return back();
+    }
+
+    public function delete(EanList $ean){
+//        dd($ean);
+        $ean->delete();
+        return back();
     }
 }
--- resources/views/skroutz/getSkroutzUrlByEan.blade.php
+++ resources/views/skroutz/getSkroutzUrlByEan.blade.php
@@ -1,5 +1,162 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>skroutz urls by ean</h1>
+    <div class="p-6 max-w-lg mx-auto bg-white flex items-center rounded-lg">
+        <form action="{{ route('eanList') }}" method="post" class="flex-auto mb-4">
+            @csrf
+            <div class="mb-4">
+                <label for="body" class="sr-only">Body</label>
+                <textarea name="ean" id="ean" cols="30" rows="4"
+                          class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('ean') border-red-500 @enderror"
+                          placeholder="Ένα ή περισσότερα EAN"></textarea>
+                @error('ean')
+                <div class="text-red-500 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+
+            <div>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Αποθήκευση
+                </button>
+            </div>
+        </form>
+    </div>
+
+
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="flex flex-col ml-24 mr-24 mt-6">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                EAN
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Επιστρεφει 1 προϊον
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Εχει γινει επεξεργασια
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Skroutz Url
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Διαγραφή</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+
+                        @foreach($ean_list as $ean)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 h-10 w-10">
+                                            {{--                                            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">--}}
+                                        </div>
+                                        <div class="ml-4">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{$ean->ean}}
+                                            </div>
+                                            <div class="text-sm text-gray-500">
+                                                {{--jane.cooper@example.com--}}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    @if($ean->returns_one_product)
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-200 text-gray-800">
+                                        @else
+                                                <span
+                                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-200 text-gray-800">
+                                        @endif
+                                                    @if($ean->returns_one_product)
+                                                        ΝΑΙ
+                                                    @else
+                                                        ΟΧΙ
+                                    @endif
+
+                                    {{--                                    <div class="text-sm text-gray-900">{{$ean->returns_one_product}}</div>--}}
+                                    {{--<div class="text-sm text-gray-500">Optimization</div>--}}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    @if($ean->is_processed)
+                                        <span
+                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-200 text-gray-800">
+                                        @else
+                                                <span
+                                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-200 text-gray-800">
+                                        @endif
+
+                                                    @if($ean->is_processed)
+                                                        ΝΑΙ
+                                                    @else
+                                                        ΟΧΙ
+                                                    @endif
+                                    </span>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    @if($ean->skroutz_url)
+                                        <a href="{{$ean->skroutz_url}}" class="text-blue-600 underline" target="_blank">Άνοιγμα</a>
+                                    @else
+                                        --
+                                    @endif
+
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <form action="{{ route('eanList', $ean) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-600 hover:text-blue-900">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                                 fill="currentColor">
+                                                <path
+                                                    d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <form action="{{ route('eanList.delete', $ean) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-500 hover:text-red-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                                 fill="currentColor">
+                                                <path fill-rule="evenodd"
+                                                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                      clip-rule="evenodd"/>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                        {{ $ean_list->links() }}
+
+
+
+                        <!-- More people... -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        {{ $ean_list->links() }}
+    </div>
+
 @endsection
